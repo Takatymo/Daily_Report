b7dd56ffbd24b150c545a4d9ecc36fc647a36bb0
unknown <tie304108@S1504C10013-T1.tisnt.tis.co.jp>
2015-05-27 15:50:42 +0900

    Add functions of delete, edit, and show the details.

--- write_reports/urls.py
+++ write_reports/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^edit_report', views.edit_report),
+    url(r'^(?P<report_id>[0-9]+)/edit_report/$', views.edit_report, name='edit_report'),
+    url(r'^create_report/$', views.create_report, name='create_report'),
+    url(r'^(?P<report_id>[0-9]+)/delete_report/$', views.delete_report, name='delete_report'),
+    url(r'^(?P<report_id>[0-9]+)$', views.show_detail_of_report, name='show_detail_of_report'),
 ]
--- write_reports/views.py
+++ write_reports/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import form_for_edit
 from .models import Report
 from datetime import datetime
@@ -26,35 +26,55 @@ def log_in(request):
         print('invalid login error message')
 """
 
-def edit_report(request, report_id=None):
+def create_report(request):
     #投稿ボタンが押されたら
     if request.method == "POST":
         if request.user.is_authenticated():
             report = Report(date=datetime.now(), user=request.user)
             form = form_for_edit(request.POST, instance=report)
             form.save()
-            return render(request, 'write_reports/show_detale_of_report.html', {'form': form})
+            return redirect('/write_reports/')
+
+        #else :
+        return render(request, '/write_reports/')
+
+    else:
+        form = form_for_edit()
+
+        return render(request, 'write_reports/edit_report.html', {'form': form})
+
+
+def edit_report(request, report_id=None):
+    #投稿ボタンが押されたら
+    if request.method == "POST":
+        if request.user.is_authenticated():
+            report = Report(id=report_id, date=datetime.now(), user=request.user)
+            form = form_for_edit(request.POST, instance=report)
+            form.save()
+            return redirect('/write_reports/')
 
         #else :
         return render(request, 'write_reports/index.html')
 
     #新規作成・修正
     else:
-        #修正画面
-        if report_id:
-            report = Report.objects.get(pk=report_id)
-            form = form_for_edit(report)
-        else:
-            form = form_for_edit()
+        report = get_object_or_404(Report, id=report_id)
+        form = form_for_edit(instance=report)
 
         return render(request, 'write_reports/edit_report.html', {'form': form})
 
 def push_report(request):
     pass
 
-def show_detale_of_report(request):
-    form = form_for_edit(request.POST)
-    return render(request, 'write_reports/show_detale_of_report.html', {'form': form})
+def show_detail_of_report(request, report_id=None):
+    report = get_object_or_404(Report, id=report_id)
+    return render(request, 'write_reports/show_detail_of_report.html', {'report': report})
 
 def delete_report(request, report_id=None):
-    pass
+    #return HttpResponse(str(report_id))
+    report = get_object_or_404(Report, id=report_id)
+    if request.user.id == report.user_id:
+        report.delete()
+        return redirect('/write_reports/')
+    else:
+        return redirect('/write_reports/', {'error_message': '権限がありません。'})
